d93861d9d056cdb8f951c2d26f8e9c98053202e9
IamFennics <andrej.darmenko@gmail.com>
2020-04-29 19:17:15 +0300

    Addin "role and authorf_role" table in DB

--- ijdb.sql
+++ ijdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Мар 30 2020 г., 15:30
+-- Время создания: Апр 29 2020 г., 19:15
 -- Версия сервера: 8.0.15
 -- Версия PHP: 7.1.22
 
@@ -31,16 +31,35 @@ SET time_zone = "+00:00";
 CREATE TABLE `author` (
   `author_id` int(11) NOT NULL,
   `author_name` varchar(255) DEFAULT NULL,
-  `author_email` varchar(255) DEFAULT NULL
+  `author_email` varchar(255) DEFAULT NULL,
+  `password` char(32) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Дамп данных таблицы `author`
 --
 
-INSERT INTO `author` (`author_id`, `author_name`, `author_email`) VALUES
-(1, 'Кевин Янк', 'thatguy@kevin_yank.com'),
-(2, 'Джоан Смитт', 'joan@exemple.com');
+INSERT INTO `author` (`author_id`, `author_name`, `author_email`, `password`) VALUES
+(1, 'Кевин Янк', 'thatguy@kevin_yank.com', 'cf4031e33086538be631fb542512cddd'),
+(3, 'Джоан Смит', 'jsmith@mail.com', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `author_role`
+--
+
+CREATE TABLE `author_role` (
+  `author_id` int(11) NOT NULL,
+  `role_id` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `author_role`
+--
+
+INSERT INTO `author_role` (`author_id`, `role_id`) VALUES
+(1, ' Администратор учётных записей');
 
 -- --------------------------------------------------------
 
@@ -83,7 +102,6 @@ CREATE TABLE `jokes` (
 
 INSERT INTO `jokes` (`id`, `joketext`, `jokedate`, `author_id`) VALUES
 (1, 'Зачем цыплёнок перешёл дорогу? Чтобы попасть на другую сторону!', '2012-04-01', 1),
-(3, '\"Королеве - подвески, Констанции - подвязки, Партосу - поддяжки...\", повторял, чтобы не перепутать д`Артаньян по дороге в Англию.', '2020-02-24', 2),
 (4, 'В Новый Год всё сбывается, даже то, что в другое время сбыть не удаётся.', '2020-03-15', 1),
 (5, 'Хочешь петь - ПЕЙ!', '2020-03-18', 1);
 
@@ -99,6 +117,34 @@ CREATE TABLE `joke_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Дамп данных таблицы `joke_category`
+--
+
+INSERT INTO `joke_category` (`joke_id`, `category_id`) VALUES
+(1, 1),
+(1, 5);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `role`
+--
+
+CREATE TABLE `role` (
+  `id` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `role`
+--
+
+INSERT INTO `role` (`id`, `description`) VALUES
+('Администратор сайта', 'Добавление, удаление и редактирование катерий'),
+('Администратор учётных записей', 'Добавление, удаление и редактирование авторов'),
+('Редактор', 'Добавление, удаление и редактирование шуток');
+
+--
 -- Индексы сохранённых таблиц
 --
 
@@ -106,7 +152,14 @@ CREATE TABLE `joke_category` (
 -- Индексы таблицы `author`
 --
 ALTER TABLE `author`
-  ADD PRIMARY KEY (`author_id`);
+  ADD PRIMARY KEY (`author_id`),
+  ADD UNIQUE KEY `author_email` (`author_email`);
+
+--
+-- Индексы таблицы `author_role`
+--
+ALTER TABLE `author_role`
+  ADD PRIMARY KEY (`author_id`,`role_id`);
 
 --
 -- Индексы таблицы `category`
@@ -127,6 +180,12 @@ ALTER TABLE `joke_category`
   ADD PRIMARY KEY (`joke_id`,`category_id`);
 
 --
+-- Индексы таблицы `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT для сохранённых таблиц
 --
 
@@ -134,13 +193,13 @@ ALTER TABLE `joke_category`
 -- AUTO_INCREMENT для таблицы `author`
 --
 ALTER TABLE `author`
-  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT для таблицы `jokes`
